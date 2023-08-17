87ddcf2e8e69a4a9bd2ed7e14ac3e4eeda9716a0
GiacomoTerra <87613279+GiacomoTerra@users.noreply.github.com>
2023-08-17 16:27:06 +0200

    Add files via upload

--- grapevine.py
+++ grapevine.py
@@ -9,15 +9,15 @@ import torch.nn.functional as F
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 from torch.utils.tensorboard import SummaryWriter
-import torchvision.transforms as transforms
+from torchvision import transforms
 from torchvision.utils import make_grid
 
 class GrapeDataset(Dataset):
 	# inizializza il Dataset object con la directory e la grandezza delle patch
-	def __init__(self, data_dir, patch_size):
+	def __init__(self, data_dir, transform=None):
 		self.data_dir = data_dir
 		self.image_files = self.load_image_files()
-		self.patch_size = patch_size
+		self.transform = transform
 	# carica su un vettore i path dei file immagini
 	def load_image_files(self):
 		image_files = []
@@ -31,36 +31,42 @@ class GrapeDataset(Dataset):
 	def __getitem__(self, index):
 		image_path = self.image_files[index]
 		image = Image.open(image_path)
-		width, height = image.size
-		patches = []
-		for i in range(0, height, self.patch_size):
-			for j in range(0, width, self.patch_size):
-				patch = image.crop((j, i , j+self.patch_size, i+self.patch_size))
-				tensor_patch = ToTensor()(patch)
-				# Calculate padding to make the patch equal to patch_size
-				padding_left = (self.patch_size - tensor_patch.size(2)) // 2
-				padding_right = self.patch_size - tensor_patch.size(2) - padding_left
-				padding_top = (self.patch_size - tensor_patch.size(1)) // 2
-				padding_bottom = self.patch_size - tensor_patch.size(1) - padding_top
-				# Apply padding to make the patch uniform size
-				padded_patch = F.pad(tensor_patch, (padding_left, padding_right, padding_top, padding_bottom))
-				patches.append(tensor_patch)
-		return patches
+		if self.transform is not None:
+			image = self.transform(image)
+		return image
 		
 	def __len__(self):
 		return len(self.image_files)
 
+# Apro un Summary Writer su TensorBoard
 writer = SummaryWriter()
+
+# Definizione delle trasformazioni
+transform = transforms.Compose([
+	# Ridimensiona l'immagine alle dimensioni desiderate
+	transforms.Resize((300, 300)), 
+	# Converte l'immagine in un tensore
+	transforms.ToTensor(),
+	transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+])
+
+# Creazione del dataset
 data_dir = "test_dataset"
-patch_size = 300
-dataset = GrapeDataset(data_dir, patch_size)
+dataset = GrapeDataset(data_dir, transform=transform)
 print(len(dataset))
-dataloader = DataLoader(dataset, batch_size = 16, shuffle = True)
-for i, data in enumerate(dataset):
-	images = data
-	images_tensor = torch.stack(images)
-	transform = transforms.Compose([transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5,0.5,0.5])])
-	transformed_images = transform(images_tensor)
-	image_grid = make_grid(transformed_images, nrow=4, normalize=True, scale_each=True)
-	writer.add_image('Images', image_grid, i)
+
+# Creazione del dataloader
+dataloader = DataLoader(dataset, batch_size = 1, shuffle = True)
+
+# Caricamento di un batch di immagini dal dataloader
+dataiter = iter(dataloader)
+images = next(dataiter)
+
+# creazione di una griglia di immagini per la visualizzazione
+img_grid = make_grid(images, nrow=4, normalize=True, scale_each=True)
+
+# Aggiunta della griglia di immagini a TensorBoard
+writer.add_images('Sample Images', img_grid)
+
+# Chiusura del SummaryWriter
 writer.close()
